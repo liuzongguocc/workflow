0cef7eda02c26843741a53fab8817e9697fe1b54
zliuzon <liuzongguo@rayootech.com>
2019-03-18 17:41:03 +0800

    add blog.html but fail to workwq

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from django.conf.urls import include
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r"^blog/", include('blog.urls', namespace='blog' , app_name='blog')),
 ]
